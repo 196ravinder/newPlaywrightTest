76ed3047334fb351720fa8f17055150a078bfc38
Ravinder Kumar <196ravinder@gmail.com>
2026-07-13 12:04:30 +0530

    Initial Commit

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -3,40 +3,38 @@ import { locatorData } from "../locators/locators";
 import { userData } from "../data/userData";
 import { basePage } from "./basePage";
 
-export class homePage extends basePage{
+export class homePage extends basePage {
 
-    constructor(page: any){
+    constructor(page: any) {
         super(page)
     }
-    async addToCart(){
+    async addToCart() {
         await this.page.locator(locatorData.homePage.pageLoadWaitFor).waitFor();
-        const products=  await this.page.locator(locatorData.homePage.selectProduct);
+        const products = await this.page.locator(locatorData.homePage.selectProduct);
         const productCount = await products.count();
-       // await this.page.locator(locatorData.homePage.selectProduct)
-       for(let i=0; i<productCount; i++){
-       if( await products.nth(i).locator("b").textContent()===userData.productName)
-       {
-       await products.nth(i).locator(locatorData.homePage.clickOnAddToCartButton).click();
-       break;
-       }
-       }
-       await this.page.locator(locatorData.homePage.clickOnCartButton).click();
-       await this.page.locator(locatorData.homePage.clickOnCheckoutButton).click();
-       await this.page.locator(locatorData.homePage.selectCountry).pressSequentially(userData.selectCountry,{delay:50});
-       const countryDropdownList= await this.page.locator(locatorData.homePage.countryDropDown);
-      await countryDropdownList.waitFor();
-      const optionsDropDownListCountry= await countryDropdownList.locator('button').count();
-
-       for(let i=0; i<optionsDropDownListCountry; i++){
-        const countryText= await countryDropdownList.locator('button').nth(i).textContent();
-        if(countryText=== userData.selectionData){
-            await countryDropdownList.locator('button').nth(i).click();
-            break;
+        // await this.page.locator(locatorData.homePage.selectProduct)
+        for (let i = 0; i < productCount; i++) {
+            if (await products.nth(i).locator("b").textContent() === userData.productName) {
+                await products.nth(i).locator(locatorData.homePage.clickOnAddToCartButton).click();
+                break;
+            }
         }
+        await this.page.locator(locatorData.homePage.clickOnCartButton).click();
+        await this.page.locator(locatorData.homePage.clickOnCheckoutButton).click();
+        await this.page.locator(locatorData.homePage.selectCountry).pressSequentially(userData.selectCountry, { delay: 50 });
+        const countryDropdownList = await this.page.locator(locatorData.homePage.countryDropDown);
+        await countryDropdownList.waitFor();
+        const optionsDropDownListCountry = await countryDropdownList.locator('button').count();
 
-       }
-       //await this.page.getByRole(locatorData.homePage.clickOnPlaceOrder).click();
-       await this.page.getByRole(locatorData.homePage.clickOnPlaceOrder.role,{ name: locatorData.homePage.clickOnPlaceOrder.name }).click();
-       console.log("test");
+        for (let i = 0; i < optionsDropDownListCountry; i++) {
+            const countryText = await countryDropdownList.locator('button').nth(i).textContent();
+            if (countryText === userData.selectionData) {
+                await countryDropdownList.locator('button').nth(i).click();
+                break;
+            }
+
+        }
+        //await this.page.getByRole(locatorData.homePage.clickOnPlaceOrder).click();
+        await this.page.getByRole(locatorData.homePage.clickOnPlaceOrder.role, { name: locatorData.homePage.clickOnPlaceOrder.name }).click();
     }
 }
